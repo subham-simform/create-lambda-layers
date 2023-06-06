40bd265866a7bd9370540d13052d5adaa30568c8
Subham Polpagedar <subham.p@simformsolutions.com>
2023-06-06 15:03:09 +0530

    Updated code

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use the official Python runtime as the base image
-FROM python:3.8-slim-buster
+FROM python:3.10-slim-buster
 
 # Set the working directory in the container
 WORKDIR /app
@@ -16,4 +16,6 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt -t /opt/python/
 
 # Set the CMD to zip the installed packages into a layer
-CMD cd /opt && zip -r9 /app/layer.zip .
+# change the `requests-layer` to the LAYER_NAME variable as per create_layer.sh file
+
+CMD cd /opt && zip -r9 /app/requests-layer.zip .
--- create_layer.sh
+++ create_layer.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 # Set the directory where the Dockerfile and requirements.txt are located
-# DIRECTORY="/path/to/your/layer/directory"
 DIRECTORY="$(pwd)"
-LAYER_NAME="layer"
+
+# Change it as per your requirement
+LAYER_NAME="requests-layer"
+
 # Build the Docker image
 docker build -t lambda-layer "$DIRECTORY"
 
 # Run the Docker container to create the layer
 docker run --name lambda-layer-container -v "$DIRECTORY:/app" lambda-layer
 
-# Move the layer.zip file to the layers directory
-mkdir layers
+# create layers directory, if not created.
+
+mkdir -p layers
+
+# Move the zip file in layers directory.
 
 mv "$DIRECTORY/$LAYER_NAME.zip" "$DIRECTORY/layers/$LAYER_NAME.zip"
 
